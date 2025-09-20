7271c14824c020763452976f4f104117db41d946
Manidweep <manidweep678@gmail.com>
2025-09-21 04:14:44 +0530

    sm6375-common: Add maintainer name overlay
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- sm6375.mk
+++ sm6375.mk
@@ -209,7 +209,8 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-matrixx
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
