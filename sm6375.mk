8fdafe22ca00fee670e20dc487a5e37ce7f82886
NoCache-69 <chethangowdasj86@gmail.com>
2025-12-20 20:10:55 +0000

    sm6375-common: Drop livedisplay
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- sm6375.mk
+++ sm6375.mk
@@ -182,10 +182,6 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.oplus
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_blair.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_blair.xml \
